614fc0c0bf3b29210117fd1d0b8a1585f9db4ae7
preheat99 <prabhatamola99@gmail.com>
2024-09-01 00:22:24 +0530

    Add code in custom card component

--- portfolio/src/shared/custom-card/custom-card.tsx
+++ portfolio/src/shared/custom-card/custom-card.tsx
@@ -1,7 +1,22 @@
+import { Card } from "@mui/material";
 import "./custom-card.scss";
+import { CardI } from "../shared-models";
 
-const CustomCard = () => {
-  return <></>;
+const CustomCard = ({
+  classes = {},
+  raised = false,
+  styles = {},
+  icon = "",
+  heading,
+  content,
+}: CardI) => {
+  return (
+    <Card classes={classes} raised={raised} sx={styles}>
+      <div className="card-icon">{icon}</div>
+      <div className="card-heading">{heading}</div>
+      <div className="card-content">{content}</div>
+    </Card>
+  );
 };
 
 export default CustomCard;

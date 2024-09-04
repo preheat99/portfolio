03a268204355bc00544adb0a309cc2ca7f3cf03b
preheat99 <prabhatamola99@gmail.com>
2024-09-04 22:42:33 +0530

    Create custom kpi

--- portfolio/src/shared/custom-kpi/custom-kpi.tsx
+++ portfolio/src/shared/custom-kpi/custom-kpi.tsx
@@ -1,7 +1,20 @@
+import { Card } from "@mui/material";
+import { KpiI } from "../shared-models";
 import "./custom-kpi.scss";
+import { useIntersectionObserver } from "../intersection-observer-context";
 
-const CustomKpi = () => {
-  return <></>;
+const CustomKpi = ({ icon, label, scrollId }: KpiI) => {
+  const observedEntries = useIntersectionObserver();
+  return (
+    <Card
+      raised={false}
+      data-viewport={`kpi-viewport${scrollId}`}
+      className={`custom-kpi animated-element ${observedEntries[`kpi-viewport${scrollId}`] ? "animate" : ""}`}
+    >
+      <div className="kpi-icon">{icon}</div>
+      <div className="kpi-label">{label}</div>
+    </Card>
+  );
 };
 
 export default CustomKpi;

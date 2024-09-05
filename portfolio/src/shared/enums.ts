08f8aa2b6a1cef647eb3dd92f913188b34ab0e4b
preheat99 <prabhatamola99@gmail.com>
2024-09-05 22:40:37 +0530

    Create headerlinks constant in enums

---
+++ portfolio/src/shared/enums.ts
@@ -0,0 +1,24 @@
+import { HeaderLinkI } from "./shared-models";
+
+export const headerLinks: HeaderLinkI[] = [
+  {
+    label: "Home",
+    id: "#home",
+  },
+  {
+    label: "About",
+    id: "#about",
+  },
+  {
+    label: "Services",
+    id: "#services",
+  },
+  {
+    label: "Projects",
+    id: "#projects",
+  },
+  {
+    label: "Contacts",
+    id: "#contacts",
+  },
+];
